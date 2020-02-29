500d9723331f958986a79f940988d4cf5019d981
rishabhgarg25699 <rishabhgarg25699@gmail.com>
2020-02-29 14:57:24 +0530

    Added

---
+++ public/shop.js
@@ -0,0 +1,30 @@
+function fetchProducts(done) {
+    $.get('/api/products', function(data) {
+        done(data)
+    })
+}
+
+function addProduct(name, manuf, price, done) {
+    $.post('/api/products', {
+        name: name,
+        manufacturer: manuf,
+        price: price
+    }, function(data) {
+        done(data)
+    })
+}
+
+function createProductCard(product) {
+    return $(`
+    <div class="col-3 row-3 card mx-4 my-2 p-4">
+        <h4 class="product-name"> ${product.name}</h4>
+        <div class="product-manufacturer">${product.manufacturer}</div>
+        <div class="row">
+            <div class="col row-4 m-3 p-4">
+                <b>Rs. ${product.price}</b>
+            </div>
+            <button class="col btn btn-primary m-3">Buy</button>
+            <button class="col btn btn-primary m-3">Add To Cart</button> 
+        </div>
+    </div>`)
+}
